2232521025e4c4325e25f2a18eedf1400aad9ea6
phongtran227 <45173350+phongtran227@users.noreply.github.com>
2018-11-19 21:54:40 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,26 @@
-self.addEventListener('push', function (event) {
-    // Received a push message 
-});
+async registerServiceWorker(applicationServerPublicKey) {
+        const applicationServerKey = this.urlB64ToUint8Array(applicationServerPublicKey);
+        const sw = await navigator.serviceWorker.register(`/service-worker.js`, { scope: '/' });
+
+        try {
+            if (Notification.permission === 'denied') {
+                return;
+            }
+            let subscription = await sw.pushManager.getSubscription();
+
+            let isSubscribed = !(subscription === null);
+
+            if (!isSubscribed) {
+                subscription = await sw.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey
+                });
+                isSubscribed = true;
+            }
+
+            return JSON.stringify(subscription.toJSON());
+        } catch (err) {
+            console.log('Failed to subscribe the user: ', err);
+            return null;
+        }
+    }
